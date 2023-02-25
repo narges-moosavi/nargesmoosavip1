4dfecdb86945059da97a254eea5777f9d90139d3
narges-moosavi <125422107+narges-moosavi@users.noreply.github.com>
2023-02-25 06:44:01 -0800

    Add files via upload

--- Untitlednarges1.cpp
+++ Untitlednarges1.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
- const int ROWS = 3;
-const int COLS = 3;
-int arr[ROWS][COLS];
-
-for (int j = 0; j < COLS; j++) {
-    for (int i = 0; i < ROWS; i++) {
-        cin >> arr[i][j];
+  float arr[3][3];
+  float det;
+  cout << "9 adad baraye araye vared konid :" << endl;
+  for(int i=0; i<3; i++) {
+    for(int j=0; j<3; j++) {
+      cin >> arr[i][j];
     }
-}
+  }
 
-for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
-        cout << arr[i][j] << " ";
-    }
-    cout << endl;
-}
+  det = arr[0][0]*(arr[1][1]*arr[2][2]-arr[2][1]*arr[1][2])-arr[0][1]*(arr[1][0]*arr[2][2]-arr[2][0]*arr[1][2])+arr[0][2]*(arr[1][0]*arr[2][1]-arr[2][0]*arr[1][1]);
+  cout << "Determinal araye : " << det << endl;
+
+  return 0;
 }
