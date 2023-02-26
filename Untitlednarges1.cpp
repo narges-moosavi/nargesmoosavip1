295c11ba5b7e4b082bdb054999e0badf2d4a84e2
narges-moosavi <125422107+narges-moosavi@users.noreply.github.com>
2023-02-26 09:39:44 +0330

    Update Untitlednarges1.cpp

--- Untitlednarges1.cpp
+++ Untitlednarges1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
   float arr[3][3];
   float det;
-  cout << "9 adad baraye araye vared konid :" << endl;
+  cout << "9 adad dar araye vared konid :" << endl;
   for(int i=0; i<3; i++) {
     for(int j=0; j<3; j++) {
       cin >> arr[i][j];
@@ -11,7 +11,7 @@ int main(){
   }
 
   det = arr[0][0]*(arr[1][1]*arr[2][2]-arr[2][1]*arr[1][2])-arr[0][1]*(arr[1][0]*arr[2][2]-arr[2][0]*arr[1][2])+arr[0][2]*(arr[1][0]*arr[2][1]-arr[2][0]*arr[1][1]);
-  cout << "Determinal araye : " << det << endl;
+  cout << "Determinan araye : " << det << endl;
 
   return 0;
 }
